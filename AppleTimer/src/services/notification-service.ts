ab7ee1f0be2076f685d4c49ed831f8e85c935ca2
Yangxi-FAN <yxfan@thoughtworks.com>
2021-06-04 23:04:31 +0800

    Stop other sounds first before playing target sound

--- AppleTimer/src/services/notification-service.ts
+++ AppleTimer/src/services/notification-service.ts
@@ -4,26 +4,38 @@ export class NotificationService {
   private readonly _3SecsCountDownSound: Sound
   private readonly _bellSound: Sound
   private readonly _startSound: Sound
+  private readonly _allSounds: Sound[]
 
   constructor() {
     this._3SecsCountDownSound = this.loadSound('3_secs_countdown.mp3')
     this._bellSound = this.loadSound('mixkit_cartoon_door_melodic_bell_110.wav')
     this._startSound = this.loadSound('start.mp3')
+    this._allSounds = [this._3SecsCountDownSound, this._bellSound, this._startSound]
   }
 
   play3SecsCountDown = () => {
-    this._3SecsCountDownSound.play(success => success || console.log('Failed to play sound'))
+    this.playSound(this._3SecsCountDownSound)
   }
 
   playBell = () => {
-    this._bellSound.play(success => success || console.log('Failed to play sound'))
+    this.playSound(this._bellSound)
   }
 
   playStart = () => {
-    this._startSound.play(success => success || console.log('Failed to play sound'))
+    this.playSound(this._startSound)
   }
 
-  private loadSound = (soundName: string) => {
+  private playSound(soundInstance: Sound) {
+    // 1.Stop all other sounds first
+    this._allSounds
+      .reduce((acc, cur) => acc.then(() => cur.stop()), Promise.resolve())
+      .then(() => {
+        // 2.Then play the target sound
+        soundInstance.play(success => success || console.log('Failed to play sound'))
+      })
+  }
+
+  private loadSound(soundName: string) {
     // Load the sound file 'whoosh.mp3' from the app bundle
     // See notes below about preloading sounds within initialization code below.
     return new Sound(soundName, Sound.MAIN_BUNDLE, error => {
