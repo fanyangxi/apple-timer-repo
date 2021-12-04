69ccf78c57708ef46474799989e7b58cc8dea4b5
Yangxi-FAN <yxfan@thoughtworks.com>
2021-12-04 16:59:52 +0800

    countdown-timer, fix issue, the stopAndReset event should be triggered after ticked event

--- AppleTimer/src/services/countdown-timer.ts
+++ AppleTimer/src/services/countdown-timer.ts
@@ -116,8 +116,8 @@ export class CountdownTimer {
         this.triggerCallback(type, counter, beforeStartDelayMilliSecs)
 
         if (counter === 0) {
-          this.stopAndReset()
           this.triggerCallback(TickingType.Finished, counter)
+          this.stopAndReset()
           resolve()
           return
         }
@@ -125,8 +125,8 @@ export class CountdownTimer {
         this._timerId = setInterval(() => {
           counter--
           if (counter === 0) {
-            this.stopAndReset()
             this.triggerCallback(TickingType.Finished, counter)
+            this.stopAndReset()
             resolve()
           } else {
             this.triggerCallback(TickingType.Ticked, counter)
