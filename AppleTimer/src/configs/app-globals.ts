7599262b46af0da894debff092e5c73e672b650c
Yangxi-FAN <yxfan@thoughtworks.com>
2021-12-06 14:23:03 +0800

    audio paly: enable background and screen-locked palying

--- AppleTimer/src/configs/app-globals.ts
+++ AppleTimer/src/configs/app-globals.ts
@@ -2,4 +2,8 @@ import Sound from 'react-native-sound'
 
 // Decide to use 'Playback':
 // - Enable play-sound in silence mode (using the Ring/Silent switch).
-Sound.setCategory('Playback', true)
+
+// Playback: play when in background, won't play when locked,
+// MultiRoute: play when in background, play when locked,
+Sound.setCategory('MultiRoute', true)
+Sound.setMode('SpokenAudio')
