b4868ba967acd2ae4e8c894702716fa65700fe3e
Yangxi-FAN <yxfan@thoughtworks.com>
2021-12-13 01:30:16 +0800

    firebase: crashlytics, add install @react-native-firebase/crashlytics

---
+++ AppleTimer/src/common/firebase-helper.ts
@@ -0,0 +1,65 @@
+export {}
+
+// Crash Log: https://rnfirebase.io/crashlytics/usage#usage
+// import firebase, { ReactNativeFirebase } from '@react-native-firebase/app'
+// import crashlytics from '@react-native-firebase/crashlytics';
+// crashlytics().log('User signed in.');
+
+// Crash Attributes: https://rnfirebase.io/crashlytics/usage#crash-attributes
+// There are various methods to set attributes for the crash report, in order to provide analytics for crashes and
+// help you review them. You can use set methods to set predefined attributes, but you can also set your own
+// custom attributes.
+// crashlytics().setUserId(user.uid),
+// crashlytics().setAttribute('credits', String(user.credits)),
+// crashlytics().setAttributes({
+//   role: 'admin',
+//   followers: '13',
+//   email: user.email,
+//   username: user.username,
+// }),
+
+// Error Reports: https://rnfirebase.io/crashlytics/usage#error-reports
+// Even if you catch unexpected errors, in order for your app to recover and behave smoothly you can still report
+// them through Crashlytics using the recordError method. This will also provide you with the associated stack trace.
+// crashlytics().recordError(error);
+
+//========================
+// // Your secondary Firebase project credentials for Android...
+// const androidCredentials = {
+//   clientId: '',
+//   appId: '',
+//   apiKey: '',
+//   databaseURL: '',
+//   storageBucket: '',
+//   messagingSenderId: '',
+//   projectId: '',
+// }
+//
+// // Your secondary Firebase project credentials for iOS...
+// const iosCredentials = {
+//   clientId: '',
+//   appId: '',
+//   apiKey: '',
+//   databaseURL: '',
+//   storageBucket: '',
+//   messagingSenderId: '',
+//   projectId: '',
+// }
+//
+// export const initiateFirebase = () => {
+//   // Select the relevant credentials
+//   const credentials: FirebaseAppOptions | undefined = Platform.select({
+//     android: androidCredentials,
+//     ios: iosCredentials,
+//   })
+//
+//   const config = {
+//     name: 'SECONDARY_APP',
+//   }
+//
+//   if (credentials === undefined) {
+//     logger.error(`Failed to load FirebaseApp credentials for mobile platform: ${Platform.OS}/${Platform.Version}`)
+//     return
+//   }
+//   firebase.initializeApp(credentials, config).catch(handleErr)
+// }
